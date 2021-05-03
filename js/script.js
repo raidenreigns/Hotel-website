c8e243f41431a5c68784eb49c5dbc5f0cfcd6e9e
Raiden <mightysimson@gmail.com>
2021-05-03 22:19:49 +0530

    new css added

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(function (){
 
 	$(".navbar-toggle").blur(function (event) {
 		var screenWidth = window.innerWidth;
-		if (screenWidth < 768) {
+		if (screenWidth < 767) {
 			$("#collapsable-nav").collapse('hide');
 		}
 	});
